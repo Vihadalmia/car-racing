e4f03cf687c5258084e462743939678cdd1f3c0d
Vihadalmia <72975944+Vihadalmia@users.noreply.github.com>
2021-02-09 15:46:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var canvas;
+var form,player,game;
+var playerCount;
+var gameState = 0;
+
+var database;
+
+function setup(){
+  canvas = createCanvas(400,400);
+database = firebase.database();
+}
+function draw(){
+
+}
